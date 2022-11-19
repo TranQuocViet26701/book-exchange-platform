71d7cc4a54682d88c3dc47d98a43a70b025fe333
quocthanhh <dangthanh3819@gmail.com>
2022-11-19 13:43:51 +0700

    sign up page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css"
 import { Container } from "react-bootstrap";
 
 export default function Login() {
+    const [fullName, setFullName] = useState("Enter Full name");
+    const [username, setUsername] = useState("Enter Username");
+    const [password, setPassword] = useState("Enter Password");
+    const [email, setEmail] = useState("Enter Email");
+
   return (
     <Container>
       <form className="Login-form">
         <div className="Login-form-content">
           <h3 className="Login-form-title">Sign Up</h3>
           <div className="form-group mt-3">
-            <label style={{display:"flex"}}>Email address</label>
+            <label style={{display:"flex"}}>Full Name</label>
             <input
               type="email"
               className="form-control mt-1"
-              placeholder="Enter email"
+              placeholder={fullName}
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label style={{display:"flex"}}>Username</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder={username}
             />
           </div>
           <div className="form-group mt-3">
@@ -21,7 +34,15 @@ export default function Login() {
             <input
               type="password"
               className="form-control mt-1"
-              placeholder="Enter password"
+              placeholder={password}
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label style={{display:"flex"}}>Email address</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder={email}
             />
           </div>
           <div className="d-grid gap-2 mt-3">

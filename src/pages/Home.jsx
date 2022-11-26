5fb5cf1b43ff44228e31c4f2c9f20299d90829c9
quocthanhh <dangthanh3819@gmail.com>
2022-11-26 10:23:44 +0700

    page Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,58 @@
-import React from 'react';
-import CardTimeLine from '../components/BookCard/cardTimeLine';
+import React, { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
+import { Card, Button } from "react-bootstrap";
+import { Row, Col } from 'react-bootstrap';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+const CardTimeLine = ({uId, bookName, fullUserName, bookId, bookImg, bookDesc}) => {
+  return (
+    <Card style={{ padding: "0", margin: "20px" }}>
+      <Card.Header as="h5" style={{ display: "flex" }}>
+        <Link to={`/user/${uId}`}>{fullUserName}</Link>
+      </Card.Header>  
+      <Card.Body>
+      <Card style={{ width: '40rem', margin: '10px', padding: '0' }}>
+            <Card.Body>
+                <Row>
+                    <Col md='4'>
+                        <Card.Img src={bookImg} />
+                    </Col>
+                    <Col md='8'>
+                        <Card.Title >{bookName}</Card.Title>
+                        <Card.Text style={{ display: 'flex' }} >{bookDesc}</Card.Text>
+                    </Col>
+                </Row>
+            </Card.Body>
+        </Card>
+        <div style={{ textAlign: "right" }}>
+          <Button variant="primary">Send Request</Button>
+        </div>
+      </Card.Body>
+    </Card>
+  );
+};
 
 function Home() {
+
+  
+  const [booksList, setBooksList] = useState([]);
+
+  useEffect(() => {
+    const getAllBooks = async () => {
+      const res = await axios.get('https://afternoon-brook-19976.herokuapp.com/book/get-all-available-books');
+      setBooksList(res.data)
+    };
+    getAllBooks();
+  },[]);
+
+  console.log("booksList", booksList)
+
   return (
-    <div>
-      <CardTimeLine />
-    </div>
+    <Container>
+      {booksList &&booksList.map(book => <CardTimeLine uId={book.uId} key={book.bookId} fullUserName={book.fullUserName} bookName={book.bookName} bookImg={book.bookImg} bookDesc={book.bookDesc}/>)}
+      
+    </Container> 
   );
 }
 
